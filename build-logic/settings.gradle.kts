d90e91ed6fed60eaebb76e455dfb2e43d579d5b6
Juan Jose Abuin <juanj.abuin@gmail.com>
2025-09-16 21:46:32 -0300

    3.7 Spring Boot service convention plugin implementation

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "build-logic"
+
+dependencyResolutionManagement {
+    repositories {
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        maven { url = uri("https://repo.spring.io/milestone") }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
